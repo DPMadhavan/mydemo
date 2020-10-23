0dc9f2514bd0b90e23c54249dbf8a2a1fc8cbd5a
DPMadhavan <71888958+DPMadhavan@users.noreply.github.com>
2020-10-23 15:37:51 +0530

    Update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -15,7 +15,7 @@ public class DemoApplication {
 	
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot! Hi Deepa" ;
+		return "Greetings from Spring Boot! Hi Deepa???" ;
 	}
 
 }
